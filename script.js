15c6a8da6f571056d728134d07cec98f279d3a4b
Marina Paskal <paskalm@rogers.com>
2023-01-07 02:21:19 -0500

    Add function startTimer

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
-console.log("hi");
-console.log(window);
+// Handle inputs
+const interval = document.querySelector(".control_interval");
+const limit = document.querySelector(".control_limit");
+const displayCounter = document.querySelector(".display_text");
+
+// Listen event
+//document.addEventListener("click", start);
+
+// Display counter
+let displayNumber = (counter, limit) => {
+  if (counter <= limit) {
+    console.log(counter);
+    displayCounter.textContent = counter;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Start
+function start() {
+  startTimer(displayNumber, interval.value, limit.value);
+}
+
+// Start timer
+function startTimer(displayNumber, interval, limit) {
+  // let interval = document.querySelector(".control_interval").value;
+  // let limit = document.querySelector(".control_limit").value;
+  let timerData = {
+    counter: 1,
+    limit: limit,
+    interval: interval,
+    displayNumber: displayNumber,
+    setInterval: function (x) {
+      if (x.displayNumber(x.counter++, x.limit)) {
+        setTimeout(x.setInterval, x.interval, x);
+      }
+    },
+  };
+  setTimeout(timerData.setInterval, timerData.interval, timerData);
+}
