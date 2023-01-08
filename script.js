4e68e81cfa01fd4ba9bb22dfa49068e7afcbc58c
Marina Paskal <paskalm@rogers.com>
2023-01-08 03:32:25 -0500

    Add main functions

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 const interval = document.querySelector(".control_interval");
 const limit = document.querySelector(".control_limit");
 const displayCounter = document.querySelector(".display_text");
+let count = true;
 
 // Listen event
 //document.addEventListener("click", start);
 
 // Display counter
 let displayNumber = (counter, limit) => {
-  if (counter <= limit) {
-    console.log(counter);
+  if (counter <= limit && count) {
+    console.log("counter ", counter);
     displayCounter.textContent = counter;
     return true;
   } else {
@@ -24,8 +25,6 @@ function start() {
 
 // Start timer
 function startTimer(displayNumber, interval, limit) {
-  // let interval = document.querySelector(".control_interval").value;
-  // let limit = document.querySelector(".control_limit").value;
   let timerData = {
     counter: 1,
     limit: limit,
@@ -38,4 +37,17 @@ function startTimer(displayNumber, interval, limit) {
     },
   };
   setTimeout(timerData.setInterval, timerData.interval, timerData);
+}
+
+// Stop timer
+function stop() {
+  count = false;
+}
+
+// Reset
+function reset() {
+  count = true;
+  interval.value = 1000;
+  limit.value = 5;
+  displayCounter.textContent = 0;
 }
